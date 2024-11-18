17bf0430ae011f5efe822104db4d4fe30b0f7ec0
MarkKra88 <maciej.krawczak88@gmail.com>
2024-11-18 21:29:28 +0000

    change main.py to be an entry point for standalone use

--- main.py
+++ main.py
@@ -1,31 +1,6 @@
-import os
-from dotenv import load_dotenv
-from epc_api import EPCAPIClient
+from app import create_app
 
-
-def main() -> None:
-    """Main function to run the script."""
-    # Load environment variables
-    load_dotenv()
-
-    # Retrieve authentication token from .env
-    authentication_token = os.getenv('authentication_token')
-    if not authentication_token:
-        raise ValueError("Authentication token not found in environment variables.")
-
-    # Initialize the EPC API client
-    client = EPCAPIClient(authentication_token=authentication_token)
-
-    # Input postcode (can be replaced with user input in the frontend)
-    postcode = 'HA0 3PS'
-
-    # Fetch addresses as a DataFrame
-    try:
-        data = client.fetch_addresses(postcode)
-        print(f"Data fetched for postcode '{postcode}':\n{data.head()}")
-    except RuntimeError as e:
-        print(e)
-
+app = create_app()
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
