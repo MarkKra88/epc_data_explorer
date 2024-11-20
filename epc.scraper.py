5b0545b5718563f7fae20ee62a6d4b2ed34b7d05
MarkKra88 <maciej.krawczak88@gmail.com>
2024-11-20 21:30:40 +0000

    Fix issue of empry response

--- epc.scraper.py
+++ epc.scraper.py
@@ -10,13 +10,14 @@ class EPCWebScraper:
     def __init__(self) -> None:
         """Initialize the scraper with base URL and headers."""
         self.base_url = "https://find-energy-certificate.service.gov.uk/find-a-certificate/search-by-postcode?postcode="
-        self.headers = {
-            "User-Agent": (
-                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-            )
-        }
-    
+        # self.headers = {
+        #     "User-Agent": (
+        #         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+        #         "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+        #     )
+        # }
+        print("base urls ", self.base_url)
+
     def fetch_html(self, postcode: str) -> str:
         """
         Fetch the HTML content for the given postcode.
@@ -25,7 +26,10 @@ class EPCWebScraper:
         :return: HTML content as a string.
         """
         url = f"{self.base_url}{postcode}"
-        response = requests.get(url, headers=self.headers)
+        # response = requests.get(url, headers=self.headers)
+        response = requests.get(url)
+        print("url: ", url)
+        print("response: ",response.status_code)
         if response.status_code != 200:
             raise RuntimeError(f"Failed to fetch data for postcode '{postcode}'. Status code: {response.status_code}")
         return response.text
@@ -41,16 +45,40 @@ class EPCWebScraper:
         
         # Extract EPC data
         results = []
-        for entry in soup.find_all("div", class_="certificate-result"):
+        table = soup.find("table")
+        body = table.find('tbody')
+        row = body.find_all("tr")
+        for entry in row:
+            print("-----start-----")
             address = entry.find("a", class_="govuk-link").text.strip()
             energy_rating = entry.find("td", class_="govuk-table__cell").text.strip()
             valid_until = entry.find("td", class_="govuk-table__cell date").text.strip()
-            
-            results.append({
-                "Address": address,
-                "Energy Rating": energy_rating,
-                "Valid Until": valid_until
-            })
+            if "Expired" in valid_until:
+                valid_until, blank, expired = valid_until.split('\n')
+                # print("valid_date: "+valid_date)
+                # # print(valid_date)
+                # print("expired: " + expired)
+            else:
+                expired = ""
+
+            # expired = entry.find_all(string="Expired")
+            print("address: " + address)
+            print("energy_rating: " + energy_rating)
+            print("valid_until: " + repr(valid_until))
+
+            print("expired: " + expired)
+
+
+        # for entry in soup.find_all("div", class_="certificate-result"):
+        #     address = entry.find("a", class_="govuk-link").text.strip()
+        #     energy_rating = entry.find("td", class_="govuk-table__cell").text.strip()
+        #     valid_until = entry.find("td", class_="govuk-table__cell date").text.strip()
+        #
+        #     results.append({
+        #         "Address": address,
+        #         "Energy Rating": energy_rating,
+        #         "Valid Until": valid_until
+        #     })
         
         # Return as a DataFrame
         return pd.DataFrame(results)
